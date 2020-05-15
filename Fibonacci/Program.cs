82e8472d36cd966c4553dc10d417e715b0691bc9
Mateus Ferreira <mateusanjos225@gmail.com>
2020-05-15 16:58:04 -0300

    Algoritmo simples de fibonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um valor n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int x = 0;
+            int y = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                x = x + y;
+                Console.WriteLine(x);
+                y = x - y;
+            }
+            Console.ReadKey();
+        }
+    }
+}
